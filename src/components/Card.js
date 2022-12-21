4f2e47c9fdf4f6be7cf1b61372ebc748c80d09a0
Gabriel Lemos <jmjgb@outlook.com>
2022-12-21 09:43:44 -0300

    Requisito 3 correção

--- src/components/Card.js
+++ src/components/Card.js
@@ -17,7 +17,7 @@ class Card extends React.Component {
     return (
       <div>
         <p data-testid="name-card">{ cardName }</p>
-        <img src={ cardImage } alt={ cardName } />
+        <img data-testid="image-card" src={ cardImage } alt={ cardName } />
         <p data-testid="description-card">{ cardDescription }</p>
         <p data-testid="attr1-card">{ cardAttr1 }</p>
         <p data-testid="attr2-card">{ cardAttr2 }</p>
